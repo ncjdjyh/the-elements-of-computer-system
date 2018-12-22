4dbc825e59eadce5c7a342438364db951723a38a
ncjdjyh <ncjdjyh@outlook.com>
2018-12-22 21:05:02 +0800

    add 06

---
+++ 06/src/_06_2/Main.java
@@ -0,0 +1,13 @@
+package _06_2;
+
+public class Main {
+    /**
+     * @Auther: ncjdjyh
+     * @Date: 2018/11/23 10:17
+     * @Description:
+     */
+    public static void main(String[] args) {
+        Trigger trigger = new Trigger();
+        trigger.__start();
+    }
+}
